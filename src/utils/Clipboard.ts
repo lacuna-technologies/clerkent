4ad7eabe975bc1bef95dcf1b5cc6154d046e1e8c
Huey <hello@huey.xyz>
2021-06-01 20:04:10 +0800

    fix clipboard bug

--- src/utils/Clipboard.ts
+++ src/utils/Clipboard.ts
@@ -1,9 +1,9 @@
 const readText = () => navigator.clipboard.readText()
 
-const isURI = (string: string) => (new RegExp(`/.*(:\\/\\/|about:|chrome:).*/`, `gi`)).test(string)
+const isURI = (string: string) => (new RegExp(/.*(:\/\/|about:|chrome:).*/, `gi`)).test(string)
 const hasTooLongWord = (string: string) => string.match(/\w+/g).sort((a, b) => b.length - a.length)[0].length > 30
 const isTooLong = (string: string) => string.length > 300
-const hasInvalidCharacters = (string: string) => (new RegExp(`/.*[<>|{}].*/`, `gi`)).test(string)
+const hasInvalidCharacters = (string: string) => (new RegExp(/.*[<>{|}].*/, `gi`)).test(string)
 
 const getPopupSearchText = async (): Promise<string> => {
   const clipboardText = await readText()
