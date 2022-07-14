7b5f7f1f9a2d35cf401aeda15292dea693b3701b
Huey <hello@huey.xyz>
2022-07-14 11:48:48 +0800

    test: fix svg transform

--- tests/svgTransform.js
+++ tests/svgTransform.js
@@ -2,9 +2,11 @@ const path = require(`path`)
 
 module.exports = {
   process(source, filename, config, options) {
-    return `module.exports = '${JSON.stringify({
-      path: path.basename(filename),
-      source,
-    })}';`
+    return {
+      code: `module.exports = '${JSON.stringify({
+        path: path.basename(filename),
+        source,
+      })}';`,
+    }
   },
 }
