df82b91df49c7454ddcc48dd810c2947faed2ec8
Huey <hello@huey.xyz>
2022-08-02 21:10:49 +0800

    fix: request cache bug in CommonLII

--- src/Background.tsx
+++ src/Background.tsx
@@ -1,17 +1,13 @@
+import { render } from 'preact'
 import { useEffect } from 'preact/hooks'
 import { browser } from 'webextension-polyfill-ts'
 import type { Downloads , Runtime } from 'webextension-polyfill-ts'
-import Messenger from '../utils/Messenger'
-import Finder from '../utils/Finder'
-import Storage from '../utils/Storage'
-import Scraper from '../utils/scraper'
-import Logger from '../utils/Logger'
-import { Helpers } from '../utils'
-
-const workingMemory = {
-  currentHighlightedCitation: null,
-  downloadCitation: null,
-}
+import Messenger from './utils/Messenger'
+import Finder from './utils/Finder'
+import Storage from './utils/Storage'
+import Scraper from './utils/scraper'
+import Logger from './utils/Logger'
+import { Helpers } from './utils'
 
 const getScraperResult = (
   targets: Finder.FinderResult[],
@@ -32,7 +28,7 @@ const getScraperResult = (
 // used by ContentScript/Highlighter for citation hover
 const viewCitation = async (port: Runtime.Port, otherProperties: Messenger.OtherProperties) => {
   const { citation, source } = otherProperties
-  workingMemory.currentHighlightedCitation = citation
+  await Storage.set(`CURRENT_HIGHLIGHTED_CITATION`, citation)
 
   const targets = Finder.findCase(citation)
 
@@ -52,7 +48,7 @@ const viewCitation = async (port: Runtime.Port, otherProperties: Messenger.Other
     return port.postMessage(noResultMessage)
   }
 
-  if(citation === workingMemory.currentHighlightedCitation){ // ignore outdated results
+  if(citation === await Storage.get(`CURRENT_HIGHLIGHTED_CITATION`)){ // ignore outdated results
     const data = result.map((r: Law.Legislation | Law.Case) => ({...targets[0], ...r}))
 
     const message = {
@@ -69,7 +65,7 @@ const viewCitation = async (port: Runtime.Port, otherProperties: Messenger.Other
 // Used by Popup
 const search = async (port: Runtime.Port, otherProperties: Messenger.OtherProperties) => {
   const { citation, source, jurisdiction } = otherProperties
-  workingMemory.currentHighlightedCitation = citation
+  await Storage.set(`CURRENT_HIGHLIGHTED_CITATION`, citation)
   const targets = Finder.findCase(citation)
 
   const noResultMessage: Messenger.Message = {
@@ -86,7 +82,7 @@ const search = async (port: Runtime.Port, otherProperties: Messenger.OtherProper
     return port.postMessage(noResultMessage)
   }
 
-  if(citation === workingMemory.currentHighlightedCitation){ // ignore outdated results
+  if(citation === await Storage.get(`CURRENT_HIGHLIGHTED_CITATION`)){ // ignore outdated results
     const data = result.map((r: Law.Legislation | Law.Case) => ({...targets[0], ...r}))
 
     const message = {
@@ -107,10 +103,11 @@ const downloadFile = async (url: string, fileName: string) => {
       saveAs: true,
       url,
     })
-  } else {
-    Logger.error(`Failed to downloadFile: url is empty`)
-  }
+    return
+  } 
+  Logger.error(`Failed to downloadFile: url is empty`)
 }
+
 
 const handleAction = (port: Runtime.Port) => async ({ action, ...otherProperties }) => {
   switch (action) {
@@ -150,10 +147,10 @@ const onReceiveMessage = (port: Runtime.Port) => (message: Messenger.Message) =>
     if (typeof message.action === `string`) {
       Logger.log(`background handling action`, message.action)
       return handleAction(port)(message)
-    } else {
+    } 
       Logger.log(`unknown action`, message.action)
       return
-    }
+    
   }
 }
 
@@ -201,6 +198,7 @@ const onDownload = (download: Downloads.DownloadItem) => {
   // }
 }
 
+
 const DEBUG_MODE = process.env.NODE_ENV === `development`
 
 const synchronousInit = () => {
@@ -225,4 +223,5 @@ const BackgroundPage = () => {
 
 synchronousInit()
 
-export default BackgroundPage
+const container = document.querySelector(`#background-root`)
+render(<BackgroundPage />, container)
--- src/Popup/components/ClipboardSuggestion.tsx
+++ src/Popup/components/ClipboardSuggestion.tsx
@@ -2,30 +2,50 @@ import { useState, useEffect, useCallback } from 'preact/compat'
 import Admonition from 'components/Admonition'
 import Clipboard from 'utils/Clipboard'
 import OptionsStorage from 'utils/OptionsStorage'
+import { browser } from 'webextension-polyfill-ts'
 
+// eslint-disable-next-line sonarjs/cognitive-complexity
 const ClipboardSuggestion = ({ query, applyClipboardText }) => {
   const [clipboardText, setClipboardText] = useState(``)
   const [enabled, setEnabled] = useState(false)
+  const [permissionGranted, setPermissionGranted] = useState(false)
 
   const onClick = useCallback(() => applyClipboardText(clipboardText), [clipboardText, applyClipboardText])
+  const onGrant = useCallback(async () => {
+    const permissionsRequest = await browser.permissions.request({
+      permissions: [`clipboardRead`],
+    })
+    setPermissionGranted(permissionsRequest)
+  }, [])
 
   useEffect(() => {
     (async () => {
-      const text = await Clipboard.getPopupSearchText()
-      if(text.length > 0){
-        setClipboardText(text)
-      }
+      const granted = await browser.permissions.contains({ permissions: [`clipboardRead`] })
+      setPermissionGranted(granted)
 
-      const enabled = await OptionsStorage.clipboardPaste.get()
-      setEnabled(enabled as boolean)
+      if(granted){
+        const enabled = await OptionsStorage.clipboardPaste.get()
+        setEnabled(enabled as boolean)
+        if(enabled){
+          const text = await Clipboard.getPopupSearchText()
+          if(text.length > 0){
+            setClipboardText(text)
+          }
+        }
+      }  
     })()
   }, [])
 
-  return (enabled && clipboardText && query !== clipboardText) ? (
+  return (enabled && !permissionGranted) ? (
+    <Admonition title="Search for copied text?" className="mb-4" onClick={onGrant}>
+      Click here to grant Clerkent permission to access your clipboard
+      so that Clerkent can automatically paste text that looks like a citation into the search box.
+    </Admonition>
+  ) : ((enabled && clipboardText && query !== clipboardText) ? (
     <Admonition title="Search for copied text?" className="mb-4" onClick={onClick}>
       <em>{clipboardText}</em>
     </Admonition>
-  ) : null
+  ) : null)
 }
 
 export default ClipboardSuggestion
