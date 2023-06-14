0974c3de126feb0cda291691c0abff69bdb68920
William O'Hanley <me@wohanley.com>
2023-06-13 20:24:02 -0700

    fix: tolerate dots in some common suffixes

--- src/utils/__tests__/Helpers.test.ts
+++ src/utils/__tests__/Helpers.test.ts
@@ -5,6 +5,7 @@ const commonAppendsTestCases = [
   [`Kong Chee Chui and others v Soh Ghee Hong`, `Kong Chee Chui v Soh Ghee Hong`],
   [`Fustar Chemicals Ltd v Ong Soo Hwa (liquidator of Fustar Chemicals Pte Ltd)`, `Fustar Chemicals v Ong Soo Hwa (liquidator of Fustar Chemicals)`],
   [`Good Property Land Development Pte Ltd (in liquidation) v Societe-Generale`, `Good Property Land Development v Societe-Generale`],
+  [`G.W.L. Properties Ltd. v. W.R. Grace & Co. of Canada Ltd.`, `G.W.L. Properties v. W.R. Grace of Canada`],
   [`PEX International Pte Ltd v Lim Seng Chye & Anor`, `PEX International v Lim Seng Chye`],
   [`SCK SERIJADI SDN BHD v ARTISON INTERIOR PTE LTD`, `SCK SERIJADI v ARTISON INTERIOR`],
   [`Wong Kwei Cheong v ABN-AMRO Bank NV`, `Wong Kwei Cheong v ABN-AMRO Bank`],
